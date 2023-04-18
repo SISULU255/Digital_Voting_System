23bfdff1263a7609be6b388ab8ca3326027e7397
SISULU255 <97722123+SISULU255@users.noreply.github.com>
2023-04-18 13:59:28 +0300

    Create Voting_System.py

---
+++ Voting_System.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+# Define the candidates
+candidates = ["Candidate 1", "Candidate 2", "Candidate 3"]
+
+# Define an empty dictionary to store the votes
+votes = {}
+
+# Define the main function to render the app
+def main():
+    st.title("Digital Voting System")
+
+    # Create the user ID input field
+    user_id = st.text_input("Enter user ID:")
+
+    # Create the candidate selection dropdown
+    candidate = st.selectbox("Select a candidate:", candidates)
+
+    # Create the buttons
+    if st.button("Cast Vote"):
+        if user_id in votes:
+            st.error("You have already voted!")
+        else:
+            votes[user_id] = candidate
+            st.success("Vote cast successfully!")
+
+    if st.button("Destroy Vote"):
+        if user_id in votes:
+            del votes[user_id]
+            st.success("Vote destroyed successfully!")
+        else:
+            st.error("You have not voted yet.")
+
+    if st.button("Display Results"):
+        if votes:
+            result_text = "Vote results:\n"
+            for candidate in candidates:
+                vote_count = list(votes.values()).count(candidate)
+                result_text += f"{candidate}: {vote_count}\n"
+            st.write(result_text)
+        else:
+            st.warning("No votes cast yet.")
+
+if __name__ == "__main__":
+    main()
